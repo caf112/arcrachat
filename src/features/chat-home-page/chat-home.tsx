765c0f20c80774d4ebf43bfddcb9e95bf6048cff
caf112 <yuu10san.14@gmail.com>
2024-07-18 15:47:09 +0900

    ar-ai-iconに変更

--- src/features/chat-home-page/chat-home.tsx
+++ src/features/chat-home-page/chat-home.tsx
@@ -22,11 +22,11 @@ export const ChatHome: FC<ChatPersonaProps> = (props) => {
           title={
             <>
               <Image
-                src={"/ai-icon.png"}
+                src={"/ar-ai-icon.png"}
                 width={60}
                 height={60}
                 quality={100}
-                alt="ai-icon"
+                alt="ar-ai-icon"
               />{" "}
               {AI_NAME}
             </>
